078863e10ee89ebada051d1ba18ea7646afca696
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-14 23:38:42 +0200

    Complete ninth task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButtonEl = document.getElementsByClassName('change-color')[0];
+const colorTextEl = document.getElementsByClassName('color')[0];
+
+changeColorButtonEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorTextEl.textContent = randomColor;
+}
