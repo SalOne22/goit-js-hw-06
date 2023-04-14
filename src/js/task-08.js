d380b5c8a767a86f40ef48573ae5f447899e7a7e
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-14 23:31:05 +0200

    Complete eight task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.getElementsByClassName('login-form')[0];
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert('All fields must be filled!');
+    return;
+  }
+
+  const userData = { email: email.value, password: password.value };
+  console.log('🚀 ~ file: task-08.js:19 ~ onFormSubmit ~ userData:', userData);
+
+  evt.currentTarget.reset();
+}
