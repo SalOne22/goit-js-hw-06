6f278cc2854fc41c54c1df4e2fc8f5cfc3b14553
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-15 14:06:20 +0200

    Fixed back for performance

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,15 +9,15 @@ const ingredients = [
 
 const fragment = document.createDocumentFragment();
 
-const ingredientsItemEls = ingredients.forEach((ingredient) => {
+const ingredientsItemEls = ingredients.map((ingredient) => {
   const ingredientNode = document.createElement('li');
 
   ingredientNode.textContent = ingredient;
   ingredientNode.classList.add('item');
 
-  fragment.appendChild(ingredientNode);
+  return ingredientNode;
 });
 
 const ingredientsEl = document.getElementById('ingredients');
 
-ingredientsEl.appendChild(fragment);
+ingredientsEl.append(...ingredientsItemEls);
